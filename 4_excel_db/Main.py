46edf0cacc5d8d63e305a5a45e1288976f81a200
yran <yran@citus.co.kr>
2022-04-25 18:54:23 +0900

    txt -> 엑셀 변환, 엑셀 -> DB 변환

---
+++ 4_excel_db/Main.py
@@ -0,0 +1,56 @@
+from lib.Sqlite import SqliteIo
+from lib.Csv import CsvIo
+from pathlib import Path
+
+csv_io = CsvIo()
+sqlite_io = SqliteIo()
+
+##################################################################
+# txt 파일 읽기
+##################################################################
+
+def read_file(file_name):
+    return open(f"./4_excel_db/{file_name}", "r", encoding='utf-8')
+
+##################################################################
+# txt 파일을 delimiter로 구분하여 배열에 저장
+##################################################################
+
+def txt_file_to_array(file, delimiter):
+    data = []
+
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        line = line.strip()
+        data.append(line.split(delimiter))
+
+    file.close()
+
+    return data
+    
+
+##################################################################
+# 파일 명 반환 : 경로, 확장자 없는 파일 명
+##################################################################
+
+def get_file_name(file):
+    return Path(file.name).stem
+
+##################################################################
+# txt 파일 - csv로 저장
+##################################################################
+
+# file = read_file("VOC_20210817.txt")
+# data = txt_file_to_array(file, "|")
+# csv_io.write(get_file_name(file), data)
+
+##################################################################
+# csv 파일 - db로 저장
+##################################################################
+
+file = csv_io.read("VOC_20210817")
+data = list(file)
+SqliteIo.insert(data)
+# SqliteIo.create()
---
+++ 4_excel_db/lib/Sqlite.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+class SqliteIo:
+
+    def __init__(self) -> None:
+        pass
+
+    ##################################################################
+    # SQLite Read
+    ##################################################################
+
+    def read():
+        try:
+            con = sqlite3.connect('./4_excel_db/test.db')        
+            cur = con.cursor()
+
+            cur.execute('select * from VOC_20210817;')
+            items = cur.fetchall()
+            print(items)
+        
+        finally:
+            con.close()
+
+    ##################################################################
+    # SQLlite Write
+    ##################################################################
+
+    def insert(data):
+    
+        SqliteIo.create()
+
+        coulmn_name = data[0]
+        del data[0]
+
+        try:
+            con = sqlite3.connect('./4_excel_db/test.db')
+            cur = con.cursor()
+            cur.executemany(f'INSERT INTO VOC_20210817({coulmn_name[0]}, {coulmn_name[1]}, {coulmn_name[2]}) VALUES(?,?,?)', data)
+            con.commit()
+            SqliteIo.read()
+        
+        finally:
+            con.close()
+
+    ##################################################################
+    # SQLlite Create
+    ##################################################################
+
+    def create():
+        try:
+            con = sqlite3.connect('./4_excel_db/test.db')
+            cur = con.cursor()
+            cur.execute('CREATE TABLE IF NOT EXISTS VOC_20210817(refID TEXT NOT NULL, POI_ID TEXT NOT NULL, tag TEXT NOT NULL)')
+            con.commit()
+        
+        finally:
+            con.close()
